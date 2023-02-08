ee238f37376f2ff18da17863fd036afedbba26a3
toni- <toni-@DESKTOP-2I3TJQ6>
2023-02-08 17:31:10 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number1 = 0;
+int number2 = 0;
+Console.WriteLine("PRIMER NÚMERO:");
+number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("SEGUNDO NÚMERO:");
+number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("LA SUMA ES:"+number1+"+"+number2+"="+(number1+number2));
+Console.WriteLine("LA RESTA ES:"+ number1+ "-"+ number2 + "=" + (number1-number2));
+Console.WriteLine("LA MULTIPLICACIÓN ES:"+ number1+ "*"+ number2 + "=" + (number1*number2));
+Console.WriteLine("LA DIVISIÓN ES:"+ number1+"/"+ number2+ "=" + (number1/number2));
+Console.WriteLine("EL RESIDUO ES:" + (number1%number2));
